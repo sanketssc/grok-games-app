b5a6f012fd23523c926a37f2c03864a86e9ab3ed
sanketssc <clasac118@gmail.com>
2025-03-08 13:39:04 +0530

    added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  // Get the IP address from the request
+  const forwardedFor = req.headers.get("x-forwarded-for");
+  console.log({ forwardedFor });
+  const ip = forwardedFor
+    ? forwardedFor.split(",")[0].trim()
+    : req.headers.get("x-real-ip") || "";
+
+  // Return the IP address
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/about/:path*",
+};
